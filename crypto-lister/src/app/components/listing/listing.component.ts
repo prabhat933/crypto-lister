11f7c8c37bf3def2fcc3b0dcf0b053f34144335c
pra <prabhat933@gmail.com>
2020-01-17 01:03:31 +0530

    add radio button

--- crypto-lister/src/app/components/listing/listing.component.ts
+++ crypto-lister/src/app/components/listing/listing.component.ts
@@ -12,55 +12,64 @@ export class ListingComponent implements OnInit {
           "list_id ": "1",
           "currency_name": "Bitcoin",
           "price": "8764.56",
-          "market_cap":"159185703078"
+          "market_cap":"159185703078",
+          "selected":"false"
         },
         {
           "list_id ": "2",
           "currency_name": "Etherium",
           "price": "164.46",
-          "market_cap":"17973959891"
+          "market_cap":"17973959891",
+          "selected":"false"
         },
         {
           "list_id ": "3",
           "currency_name": "Litecoin",
           "price": "57.39",
-          "market_cap":"3665462926"
+          "market_cap":"3665462926",
+          "selected":"false"
         },
         {
           "list_id ": "4",
           "currency_name": "Tether",
           "price": "0.999434",
-          "market_cap":"4630402187"
+          "market_cap":"4630402187",
+          "selected":"false"
         },
         {
           "list_id ": "5",
           "currency_name": "Iota",
           "price": "0.222148",
-          "market_cap":"617468232"
+          "market_cap":"617468232",
+          "selected":"false"
         },
         {
           "list_id ": "6",
           "currency_name": "XRP",
           "price": "0.233855",
-          "market_cap":"10208672134"
+          "market_cap":"10208672134",
+          "selected":"false"
         },
         {
           "list_id ": "7",
           "currency_name": "Bitcoin Cash",
           "price": "329.8",
-          "market_cap":"6010578194"
+          "market_cap":"6010578194",
+          "selected":"false"
         },
         {
           "list_id ": "8",
           "currency_name": "Eos",
           "price": "3.71",
-          "market_cap":"3518044873"
+          "market_cap":"3518044873",
+          "selected":"false"
         },
         {
           "list_id ": "9",
           "currency_name": "Steller",
           "price": "0.053899",
-          "market_cap":"1077371771"
+          "market_cap":"1077371771",
+          "selected":"false"
         },
       ]
     
