cb12de70019613cdde4235dd20852696829aee41
pra <prabhat933@gmail.com>
2020-01-23 12:35:53 +0530

    logic for 24 hour data

--- crypto-lister/src/app/components/price-chart/price-chart.component.ts
+++ crypto-lister/src/app/components/price-chart/price-chart.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "src/app/service/data.service";
 
 @Component({
-  selector: 'app-price-chart',
-  templateUrl: './price-chart.component.html',
-  styleUrls: ['./price-chart.component.css']
+  selector: "app-price-chart",
+  templateUrl: "./price-chart.component.html",
+  styleUrls: ["./price-chart.component.css"]
 })
 export class PriceChartComponent implements OnInit {
+  constructor(private dataservice: DataService) {}
 
-  constructor() { }
-
+  public cryptoName = "bitcoin";
   ngOnInit() {
+    this.dataservice
+      .get24HourData(this.cryptoName)
+      .subscribe(data => console.log(data));
   }
-
 }
--- crypto-lister/src/app/service/data.service.ts
+++ crypto-lister/src/app/service/data.service.ts
@@ -1,23 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataService {
-  private _url2:string = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1";
-  private _url3:string = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1";
-  constructor(private http:HttpClient) { }
-  
-  getData(perPage =5 ,pageNumber = 1,orderBy= "market_cap_desc") {
-    let _url:string = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=${orderBy}&per_page=${perPage}&page=${pageNumber}&sparkline=false`;
+  private _url2: string =
+    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1";
+  constructor(private http: HttpClient) {}
+
+  getData(perPage = 5, pageNumber = 1, orderBy = "market_cap_desc") {
+    let _url: string = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=${orderBy}&per_page=${perPage}&page=${pageNumber}&sparkline=false`;
     return this.http.get(_url);
   }
-  getCompareData(){
+  getCompareData() {
     return this.http.get(this._url2);
   }
-  get24HourData(){
-    return this.http.get(this._url3);
+  get24HourData(cryptoName = "bitcoin") {
+    let _url3: string = `https://api.coingecko.com/api/v3/coins/${cryptoName}/market_chart?vs_currency=usd&days=1`;
+
+    return this.http.get(_url3);
   }
 }
-
